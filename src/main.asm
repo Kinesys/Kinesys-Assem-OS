d7c8ce6c29d802785de1638915d927d1aa52efb4
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-02-09 11:30:10 +0900

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,40 +1,8 @@
-[bits 16]   ;16bit binary format
-[org 0x7c00] ;BIOS  Bootloder Address
+[bits 16]
+[org 0x7c00]
 
 start:
 
     xor ax, ax
 
-    mov ds, ax
-    mov es, ax
-    mov bx, 0x8000
-    mov si, kinesys_os ;source index를 kinesys os 문자열 인덱스 포인트로 설정
-
-    call kinesys_os ;kinesys_os function 호출
-
-;define function 위치
-
-kinesys_os:
-
-    mov ah, 0x0E
-
-.repeat_next_char:
-
-    lodsb
-    cmp al, 0
-    je .done_os
-
-    int 0x10
-
-    jmp .repeat_next_char
-
-.done_os:
-
-    ret
-
-times (510 - ( $ -$$ )) db 0x00 
-
-
-dw 0xAA55
-
-
+    
